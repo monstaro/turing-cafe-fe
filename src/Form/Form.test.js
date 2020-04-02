d470f534fc284df00a2f43a3342b1a0dedcb1691
monstaro <monstaro32@gmail.com>
2020-04-02 11:07:12 -0600

    tests

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -13,13 +13,18 @@ describe('Form', () => {
     fireEvent.change(getByPlaceholderText('Name'), {
       target: {value: 'mockUser'}
     })
+    expect(getByPlaceholderText('Name').value).toBe('mockUser')
     fireEvent.change(getByPlaceholderText('Time'), {
       target: {value: '5:55'}
     })
+    expect(getByPlaceholderText('Time').value).toBe('5:55')
     fireEvent.change(getByPlaceholderText('Number of Guests'), {
       target: {value: 6}
     })
-    fireEvent.click(getByText('Make Reservation'))
+    expect(getByPlaceholderText('Number of Guests').value).toBe('6')
+    // expect(getByText('mockUser')).toBeInTheDocument()
+    // fireEvent.click(getByText('Make Reservation'))
+
     // const headline = await waitFor(() => getByText('Turing Cafe Reservations'))
     // expect(headline).toBeInTheDocument()
     // const reservation = await waitFor(() => getByText('Leta'))
