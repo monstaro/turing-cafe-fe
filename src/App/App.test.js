70521f8c8e3631d226bb2e9ac3379974b1caf55f
monstaro <monstaro32@gmail.com>
2020-04-02 10:19:56 -0600

    Tests fetch is displaying things on page

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render, waitFor } from '@testing-library/react'
+import '@testing-library/jest-dom';
+
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('App', () => {
+
+  it('Renders a headline', async () => {
+    const { getByText } = render(<App />)
+    const headline = await waitFor(() => getByText('Turing Cafe Reservations'))
+    expect(headline).toBeInTheDocument()
+    const reservation = await waitFor(() => getByText('Leta'))
+    expect(reservation).toBeInTheDocument()
+  })
+})
