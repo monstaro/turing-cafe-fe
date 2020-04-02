9f8aafcc3e3185faac1e84f5be3213ae62c23009
monstaro <monstaro32@gmail.com>
2020-04-02 10:19:34 -0600

    Init commit

---
+++ src/Form/Form.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+      resInfo: {
+  
+      }
+    }
+  }
+  render() {
+    return (
+      'hi'
+    )
+  }
+}
+
+export default Form;
