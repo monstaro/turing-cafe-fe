01c21d2589d84b91abaf6be9a5124a3f12ed5825
monstaro <monstaro32@gmail.com>
2020-04-02 09:58:46 -0600

    Creates fetch func

--- src/ApiCalls/apiCalls.js
+++ src/ApiCalls/apiCalls.js
@@ -1,6 +1,5 @@
-import React from 'react';
 
 export const fetchReservations = () => {
   return fetch('http://localhost:3001/api/v1/reservations')
-  .then(response => response.json)
+  .then(response => response.json())
 }
